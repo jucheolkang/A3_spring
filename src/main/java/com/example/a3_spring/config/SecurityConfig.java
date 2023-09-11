d1af65b34f3d1853475e376081aab48dc47b48af
jucheolkang <jucheol9877@naver.com>
2023-09-11 17:34:11 +0900

    update security

--- src/main/java/com/example/a3_spring/config/SecurityConfig.java
+++ src/main/java/com/example/a3_spring/config/SecurityConfig.java
@@ -1,12 +1,25 @@
 package com.example.a3_spring.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.boot.web.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
 
 }
